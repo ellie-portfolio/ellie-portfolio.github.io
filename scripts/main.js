18bc548a1416f0b35be31b92f2f5232162392876
wikihowadventurecruise <richardbarr1991@gmail.com>
2023-07-26 16:35:36 +0200

    fixed mobile heaer design

--- scripts/main.js
+++ scripts/main.js
@@ -68,14 +68,14 @@ function playOnHover2(item, index) {
 // Click overlays to show fullscreen video (mobile-only)
 
 // Check if the device supports touch events
-const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+// const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 
-gridItems.forEach(item, index => {
-    item.addEventListener('click', fully(index));
-});
+// gridItems.forEach(item, index => {
+//     item.addEventListener('click', fully(index));
+// });
 
-function fully(index) {
-    // let bigScreen = window.matchMedia(`(min-width: 701px)`);
-    // if (bigScreen.matches) return;
-    gridVids[index].requestFullscreen();
-}
+// function fully(index) {
+//     // let bigScreen = window.matchMedia(`(min-width: 701px)`);
+//     // if (bigScreen.matches) return;
+//     gridVids[index].requestFullscreen();
+// }
